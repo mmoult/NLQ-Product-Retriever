098d27235f4a00f1f9d6f57b6c92dee60c16ed89
Matthew Moulton <30711895+mmoult@users.noreply.github.com>
2021-10-01 15:32:38 -0600

    Updated the SQL file
    Also creates the housing table. And I fixed a typo in the git ignore.

--- load_sql.py
+++ load_sql.py
@@ -1,17 +1,62 @@
 '''
 Created on Oct 1, 2021
 
-@author: moultmat
+@author: Matthew Moulton
 '''
 
 import sqlite3
 
-if __name__ == '__main__':
+def commitAction(action):
     con = sqlite3.connect('product_qa.db')
     cur = con.cursor()
-    '''
-    cur.execute('CREATE TABLE stocks
-               (date text, trans text, symbol text, qty real, price real)')
-    '''
+    
+    action(cur)
+    
     con.commit() # Commit changes
     con.close() # close after we are done
+
+
+def buildTables(cursor):
+    cursor.execute('''
+        CREATE TABLE "Jewelry" (
+            "ref" INTEGER NOT NULL UNIQUE,
+            "category" TEXT,
+            "price" INTEGER,
+            "tags" TEXT,
+            "description" TEXT,
+            "image" TEXT,
+            PRIMARY KEY("ref")
+        )''')
+    cursor.execute('''
+        CREATE TABLE "Housing" (
+            "suburb" TEXT,
+            "address" TEXT NOT NULL,
+            "rooms" INTEGER,
+            "type" TEXT,
+            "price" INTEGER NOT NULL,
+            "method" TEXT,
+            "distance" NUMERIC,
+            "postcode" INTEGER,
+            "bedrooms" INTEGER,
+            "bathrooms" INTEGER,
+            "cars" INTEGER,
+            "landsize" INTEGER,
+            "building_area" INTEGER,
+            "year_built" INTEGER,
+            "council" TEXT,
+            "latitude" NUMERIC,
+            "longitude" NUMERIC,
+            "region" TEXT,
+            "property_count" INTEGER,
+            PRIMARY KEY("address")
+        )''')
+
+
+def loadTables(cursor):
+    #cursor.execute('''INSERT INTO Jewelry...''')
+    pass
+
+
+if __name__ == '__main__':
+    commitAction(buildTables)
+    commitAction(loadTables)
