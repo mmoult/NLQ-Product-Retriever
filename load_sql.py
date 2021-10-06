7e885a7dd0e5321420bcdbbbc166fc255f63c1d1
Matthew Moulton <30711895+mmoult@users.noreply.github.com>
2021-10-06 15:30:58 -0600

    Change of Table Structure
    I made a little class for table to abstract away some of the data that would otherwise be repeated.
    
    Please create the other tables in the Table class format.

--- load_sql.py
+++ load_sql.py
@@ -5,16 +5,25 @@ Created on Oct 1, 2021
 '''
 
 import sqlite3
+import string
 
-jewelry = [
+
+class Table(object):
+    def __init__(self, name:string, primKey:int, *args):
+        self.name = name
+        self.primKey = primKey
+        self.dat = args
+
+
+jewelry = Table("Jewelry", 0,
     ["ref", "TEXT NOT NULL UNIQUE"],
     ["category", "TEXT"],
     ["price", "INTEGER"],
     ["tags", "TEXT"],
     ["description", "TEXT"],
-    ["image", "TEXT"],
-]
-housing = [
+    ["image", "TEXT"]
+)
+housing = Table("Housing", 1,
     ["suburb", "TEXT"],
     ["address", "TEXT NOT NULL"],
     ["rooms", "INTEGER"],
@@ -34,7 +43,8 @@ housing = [
     ["longitude", "NUMERIC"],
     ["region", "TEXT"],
     ["property_count", "INTEGER"]
-]
+)
+
 
 def commitAction(action):
     con = sqlite3.connect('product_qa.db')
@@ -46,28 +56,57 @@ def commitAction(action):
     con.close() # close after we are done
 
 
-def buildCreate(name, struct, primaryKey):
-    cmd = 'CREATE TABLE "' + name + '" ('
-    for attr in struct:
+def buildCreate(table:Table):
+    cmd = 'CREATE TABLE "' + table.name + '" ('
+    for attr in table.dat:
         cmd += '"' + attr[0] + '" ' + attr[1] + ','
-    cmd += ('PRIMARY KEY("' + primaryKey + '")')
+    cmd += ('PRIMARY KEY("' + table.dat[table.primKey][0] + '")')
     cmd += ')'
     return cmd
 
 
 def buildTables(cursor):
-    cursor.execute(buildCreate("Jewelry", jewelry, jewelry[0][0]))
-    cursor.execute(buildCreate("Housing", housing, housing[1][0]))
+    cursor.execute(buildCreate(jewelry))
+    cursor.execute(buildCreate(housing))
 
 
-def loadTables(cursor):
+def splitWithStrings(line: string) -> [string]:
+    
+    pass
+
+def unfinishedString(line:string) -> bool:
+    start = 0
+    unfinished = False
+    while True:
+        try:
+            start = line.index('"', start)
+            # if the index immediately before is a \, then this has been escaped
+            if start > 1 and line[start - 1] != '\\':
+                unfinished = not unfinished
+        except ValueError:
+            # Occurs when no more ". Stop looking.
+            break
+    return unfinished
+
+
+def loadTables(cursor, table:Table):
     jewelry = open('Datasets/Product-QA/Datasets/cartier_catalog.csv')
-    title = False
+    title = False   
+    sumLine = '' # this is what we will use when an entry spans several lines
+    unfinished = False
     for line in jewelry:
         if not title:
             title = True
             continue
-        comps = line.split(',')
+        
+        sumLine += line
+        # If there are an even number of non-escaped ", then we know that the line is done
+        unfinished = unfinishedString(line) ^ unfinished
+        if unfinished:
+            continue # get the next line to continue what we have
+        
+        # If we are here, the line is considered finished
+        comps = splitWithStrings(line)
         cursor.execute('''INSERT INTO Jewelry...''')   
     jewelry.close()
     pass
